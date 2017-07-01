52ccbb6c642633ff07fd646bddb4fabb126afa0b
Sam Rolfe <scrolfe@gmail.com>
2017-07-01 14:30:18 -0500

    added .get and created express and mongoose dependencies

--- server/controllers/ItemController.js
+++ server/controllers/ItemController.js
@@ -6,6 +6,19 @@ var User = require('../models/User');
 
 router.use(bodyParser.urlencoded({extended: true}));
 
+router.get('/all', function(request, response){
+    Item.find(function(error, items){
+        response.json(items)
+    });
+});
+
+router.get('/:id', function(request, response){
+    var id = request.params.id;
+    Item.findById(id, function(error, item){
+        response.json(item)
+    });
+});
+
 router.post('/', function(request, response){
     var item = new Item({
         name: request.body.name,
@@ -13,9 +26,9 @@ router.post('/', function(request, response){
         image: request.body.image,
         description: request.body.description,
         owner: request.body.owner
+        item.save();
+        response.json(item);
     });
-    item.save();
-    response.json(item);
 });
 
 router.patch('/:id', function(request, response){
