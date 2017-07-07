a47507f0c09be1a09fc18c5bd39c69525f8fe4e4
AceCat <myers.benn@gmail.com>
2017-07-07 11:20:07 -0500

    I guess this works now

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -111,6 +111,7 @@ submitButton.click(function(){
 // })
 //these activate the open item list or closed item list
 
+$('.closedItem').hide();
 
 $('#openButton').click(function(){
 	$('.openItem').show()
@@ -121,6 +122,7 @@ $('#closedButton').click(function(){
 	$('.openItem').hide();
 });
 //this closes or opens an item if the user is logged in
+
 
 $('#buttonOpen').click(function(){
 	$('.closedItem').attr('class','.openItem')
